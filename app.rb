84dd67ca6db36ff9a298b554fc07cbb6e35c8dcd
sergezolo <zolotukhinsereg@gmail.com>
2020-06-10 15:52:17 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/' do
+        "Hello, World!"
+    end
+
+    get '/name' do
+        "My name is Sergey."
+    end
+
+    get '/hometown' do
+        "My hometown is Karaganda, Kazakhstan."
+    end
+
+    get '/favorite-song' do
+        "My favorite song is My Way by Frank Sinatra."
+    end
+
 end
