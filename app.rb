e4ea0a573a783519f3da4d1eb6e0e72ba6ae187a
Jashema Panapa <jcpanapa@gmail.com>
2020-06-12 05:29:54 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/name' do 
+    "My name is Jashema"
+  end 
+  
+  get '/hometown' do 
+    "My hometown is Sumter, SC."
+  end 
+  
+  get '/favorite-song' do 
+    "My favorite song is any song that moves my soul."
+  end 
 end
