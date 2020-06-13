94270079ed02b663e3fd05e9af65ddd7c7091196
Allan Farca <allan.farca@gmail.com>
2020-06-13 04:01:47 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is Allan."
+  end
+
+  get '/hometown' do
+    "My hometown is Concord."
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Can't Hold Us."
+  end
+
 end
