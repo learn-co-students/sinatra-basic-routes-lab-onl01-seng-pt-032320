712822dffd8c13ae7066731c572f209ca771ae6c
joshpled <joshuapleduc@gmail.com>
2020-06-13 22:53:06 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/' do
+        "Hello, World!"
+    end
+
+    get '/name' do
+        "My name is Josh"
+    end
+
+    get '/hometown' do
+        "My hometown is Harker Heights"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is 911"
+    end
+
 end
