29918ea131454d6f99114cebfd79fd816babe1ae
Caroline Longnecker <cvrieous@gmail.com>
2020-06-15 04:48:26 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is Caroline Longnecker"
+  end
+  
+  get '/hometown' do
+    "My hometown is Ankeny"
+  end
+  
+  get '/favorite-song' do
+    "My favorite song is Delicate, Petite, And Other Things I'll Never Be"
+  end
 end
