d9df1241fc471411b5a057374afd6dfa73d68830
jpv2018 <jason.viola@mavs.uta.edu>
2020-06-23 14:49:10 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/' do
+        "This is about me"
+    end
+
+    get '/name' do
+        "My name is Jason"
+    end
+
+    get '/hometown' do
+        "My hometown is Arlington"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is Kings and Queens"
+    end
+
 end
