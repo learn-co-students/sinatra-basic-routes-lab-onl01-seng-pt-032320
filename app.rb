d7722ae59d373ee3cb759c3c2104622874a26b88
Bryce B <bryce.boddy@gmail.com>
2020-06-25 06:17:13 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do 
+    "Hello, World!"
+  end
+  
+  get '/name' do 
+    "My name is Leroy Jenkins"
+  end
+  
+  get '/hometown' do 
+    "My hometown is Chiraq"
+  end
+  
+  get '/favorite-song' do 
+    "My favorite song is Rick Astley - Never Gonna Give You Up"
+  end
 end
